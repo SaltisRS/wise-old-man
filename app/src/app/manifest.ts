d7fb06238bdab9484ee9bc78acc33194b41e32c0
Saltis <SaltisRS>
2026-09-21 17:31:16 +0200

    Allow any orientation for installed PWA

--- app/src/app/manifest.ts
+++ app/src/app/manifest.ts
@@ -9,7 +9,7 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: "/",
     scope: "/",
     display: "standalone",
-    orientation: "portrait",
+    orientation: "any",
     background_color: "#101319",
     theme_color: "#101319",
     icons: [
